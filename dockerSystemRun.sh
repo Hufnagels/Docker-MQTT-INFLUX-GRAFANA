3f8c512945f5e9445c6f73a4b63b60b94e981fcd
Várkonyi István <kbvconsulting@gmail.com>
2021-04-02 20:04:12 +0200

    bash script

---
+++ dockerSystemRun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /Users/pisti/Library/Mobile\ Documents/com~apple~CloudDocs/Projects/smarthome/Docker-MQTT-INFLUX-GRAFANA/
+
+if [ "$1" == "start" ]; 
+then 
+    docker-compose up -d --no-recreate
+elif [ "$1" == "stop" ];
+then 
+    docker-compose down
+else
+    echo "Unrecognized verb",$1
+fi
