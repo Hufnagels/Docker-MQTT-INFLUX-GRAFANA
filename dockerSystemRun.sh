b897201ba6b757b2ecfc37210582a77c60156a8d
Hufnagels <kbvconsulting@gmail.com>
2021-04-16 20:44:11 +0200

    External network
    Remove HA section and add external network with IP range
    Not yet dns comp

--- dockerSystemRun.sh
+++ dockerSystemRun.sh
@@ -3,11 +3,13 @@
 cd /Users/pisti/Library/Mobile\ Documents/com~apple~CloudDocs/Projects/smarthome/Docker-MQTT-INFLUX-GRAFANA/
 
 if [ "$1" == "start" ]; 
-then 
+then
+    docker network create -d bridge --subnet 192.168.1.0/24 --gateway 192.168.1.1 --ip-range 192.168.1.225/29 smarthome
     docker-compose up -d --no-recreate 
 elif [ "$1" == "stop" ];
 then 
     docker-compose down
+    docker network rm smarthome
 else
     echo "Unrecognized verb",$1
 fi
